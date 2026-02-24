51f9d1d0545d8539ef3199fced81c13610ac05d2
Ugnė Miklovaitė <136790656+mmiklovaitemm@users.noreply.github.com>
2026-02-24 20:06:49 +0200

    Set base for Netlify and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig(() => ({
   plugins: [react()],
-  base: "/",
+  base: process.env.DEPLOY_TARGET === "gh" ? "/bd-shop/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
