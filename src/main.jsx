44e7ce55cfaeca661299dbcf9812407a11a241c0
Ugnė Miklovaitė <136790656+mmiklovaitemm@users.noreply.github.com>
2026-02-25 15:46:55 +0200

    deleted helmet

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { FavoritesProvider } from "./context/FavoritesContext.jsx";
-import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <FavoritesProvider>
-          <App />
-        </FavoritesProvider>
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
