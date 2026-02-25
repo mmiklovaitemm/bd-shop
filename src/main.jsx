414235f3634970e1e90d127ac12742425579353b
Ugnė Miklovaitė <136790656+mmiklovaitemm@users.noreply.github.com>
2026-02-25 12:34:26 +0200

    add product SEO meta

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { FavoritesProvider } from "./context/FavoritesContext.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <FavoritesProvider>
-        <App />
-      </FavoritesProvider>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <FavoritesProvider>
+          <App />
+        </FavoritesProvider>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>,
 );
