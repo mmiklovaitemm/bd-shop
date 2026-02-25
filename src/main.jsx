2d50682e900f30ed9454e1485eff93e6895d6f06
Ugnė Miklovaitė <136790656+mmiklovaitemm@users.noreply.github.com>
2026-02-25 11:41:03 +0200

    fix redirects

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { FavoritesProvider } from "./context/FavoritesContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <FavoritesProvider>
         <App />
       </FavoritesProvider>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
+/* eslint-env node */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
+const repoName = "bd-shop"; // <-- jei repo pavadinimas kitoks, pakeisk čia
+
 export default defineConfig(() => ({
   plugins: [react()],
-  base: process.env.DEPLOY_TARGET === "gh" ? "/bd-shop/" : "/",
+  // GitHub Pages: https://username.github.io/bd-shop/
+  // Netlify: https://site.netlify.app/
+
+  base: process.env.DEPLOY_TARGET === "gh" ? `/${repoName}/` : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
