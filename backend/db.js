d2319e3cacc88c613208ab9182cf486fd2d122eb
Ugnė Miklovaitė <136790656+mmiklovaitemm@users.noreply.github.com>
2026-03-20 17:53:57 +0200

    connect railway db

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,6 @@
 import mysql from "mysql2/promise";
 import "dotenv/config";
 
-const pool = mysql.createPool({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "bd_shop",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 10000,
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 export default pool;
