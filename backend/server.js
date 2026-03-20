805075ef9290ac6f1fc0ca4c4c7c7b575312afa0
Ugnė Miklovaitė <136790656+mmiklovaitemm@users.noreply.github.com>
2026-03-20 17:28:14 +0200

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -11,18 +11,10 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
-import cors from "cors";
 
 import db from "./db.js";
 
 const app = express();
-
-app.use(
-  cors({
-    origin: "https://bd-shop-gray.vercel.app",
-    credentials: true,
-  }),
-);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -62,6 +54,7 @@ app.use("/uploads", express.static(uploadsDir));
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:4173",
+  "https://bd-shop-gray.vercel.app",
   process.env.FRONTEND_ORIGIN,
 ].filter(Boolean);
 
