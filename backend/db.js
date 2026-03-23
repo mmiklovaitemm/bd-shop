91b3fc60f871bf6181bf0431e63d626172cb2cdf
Ugnė Miklovaitė <136790656+mmiklovaitemm@users.noreply.github.com>
2026-03-23 13:17:49 +0200

    fixed code

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,15 @@
 import mysql from "mysql2/promise";
 import "dotenv/config";
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in .env");
+}
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 export default pool;
